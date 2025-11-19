e58f0f24eeec9d2b6a15b24c5ac8725fe04e78e2
dennis pasquali <dennispasquali206@gmail.com>
2025-11-19 16:53:53 +0100

    modifica dashboard con dati login

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,20 @@
+import { Navigate, useLocation ,useNavigate,type Location} from "react-router-dom";
+import type { ILogNavigateState } from "./Login";
+import { useEffect } from "react";
+
+
+
 export default function Dashboard() {
+    const location: Location<ILogNavigateState>=useLocation();
+    const navigate=useNavigate();
+    const userName=location.state?.userName ?? "guest";
+
+    useEffect(()=>{
+        if(!location.state) navigate('/login');
+    },[location,navigate]);
     return (
-        <p>Dashboard:</p>
+        <h1>Dashboard Page: {userName}</h1>
     )
+
+    
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,52 @@
-import { useState, type FormEvent} from "react";
+import { useState, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 
+// Tipizzazione dello state da passare a Dashboard
+export interface ILogNavigateState {
+  userName: string;
+  email: string;
+}
+
 export default function Login() {
-    const [userName,setUserName]=useState<string>("");
-    const navigate=useNavigate();
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>(""); // opzionale se vuoi inserire email
+  const navigate = useNavigate();
 
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
+    const stateToSend: ILogNavigateState = {
+      userName,
+      email
+    };
 
-    function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        //navigate(`/dashboard/${userName}`);
-        navigate(`/dashboard`,{
-            state: {userName}
-        })
+    navigate("/dashboard", { state: stateToSend });
+  }
 
-    }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Login Form</h1>
 
-    return (
-        <form onSubmit={(e)=>{handleSubmit(e)}}>
-            <h1>Login Form</h1>
-            <input type="text" placeholder="inserisci user name" value={userName}  onChange={(e) => setUserName(e.target.value)}/>
-            <p>username: {userName}</p>
-            <button type="submit">Login</button>
-        </form>
-    )
+      <input
+        type="text"
+        placeholder="Inserisci username"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        required
+      />
+
+      <input
+        type="email"
+        placeholder="Inserisci email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+
+      <p>Username: {userName}</p>
+      <p>Email: {email}</p>
+
+      <button type="submit">Login</button>
+    </form>
+  );
 }
